6284af3d66d82a68a076fca13e677061f0ad40f7
Ion Pana <pana.ion@gmail.com>
2016-01-17 10:39:21 -0500

    p45.

--- GeoQuiz/app/src/main/java/pana/ion/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/pana/ion/geoquiz/QuizActivity.java
@@ -8,24 +8,80 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+
+    private TrueFalse[] mQuestionBank = new TrueFalse[] {
+            new TrueFalse(R.string.question_oceans, true),
+            new TrueFalse(R.string.question_mideast, false),
+            new TrueFalse(R.string.question_africa, false),
+            new TrueFalse(R.string.question_americas, true),
+            new TrueFalse(R.string.question_asia, true)
+    };
+
+    private int mCurrentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                checkAnswer(true);
             }
         });
+
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkAnswer(false);
+            }
+        });
+
+        mNextButton = (Button) findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nextQuestion();
+            }
+        });
+
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+        nextQuestion();
+    }
+
+    private int getQuestionTextId() {
+        return mQuestionBank[mCurrentIndex].getQuestion();
+    }
+
+    private void checkAnswer(boolean userPressedTrue) {
+        int toastMessage;
+        if (mQuestionBank[mCurrentIndex].isTrueQuestion() == userPressedTrue) {
+            toastMessage = R.string.correct_toast;
+        } else {
+            toastMessage = R.string.incorrect_toast;
+        }
+        Toast.makeText(this, toastMessage, Toast.LENGTH_SHORT).show();
+    }
+
+    private void nextQuestion() {
+        mCurrentIndex++;
+        if (mCurrentIndex >= mQuestionBank.length) {
+            mCurrentIndex = 0;
+        }
+        mQuestionTextView.setText(getQuestionTextId());
     }
 
     @Override
@@ -35,18 +91,4 @@ public class QuizActivity extends AppCompatActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
